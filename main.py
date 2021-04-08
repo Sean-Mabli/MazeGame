50462e96bbe48294cf6abbbb6af1413add8a50bd
Sean Mabli <>
2021-04-08 23:53:49 +0000

    .

--- main.py
+++ main.py
@@ -13,22 +13,24 @@ done = False
 
 while not done:
   for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      done = True
     if(Screen == 'start'):
       Display.fill((0, 0, 0))
       pygame.draw.rect(Display, (255, 255, 255), (150, 300, 200, 60),  2)
+      Display.blit(pygame.font.SysFont("Raleway", 40).render("Conplete The Maze", 1, (255, 255, 255)), (174, 317))
+      Display.blit(pygame.font.SysFont("Raleway", 40).render("Start Game", 1, (255, 255, 255)), (174, 317))
 
     if(Screen == 'game'):
-      if event.type == pygame.QUIT:
-        done = True
-        if event.type == pygame.KEYDOWN:
-          if event.key == pygame.K_LEFT:
-            PlayerX -= 1
-          if event.key == pygame.K_RIGHT:
-            PlayerX += 1
-          if event.key == pygame.K_UP:
-            PlayerY -= 1
-          if event.key == pygame.K_DOWN:
-            PlayerY += 1
+      if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT:
+          PlayerX -= 1
+        if event.key == pygame.K_RIGHT:
+          PlayerX += 1
+        if event.key == pygame.K_UP:
+          PlayerY -= 1
+        if event.key == pygame.K_DOWN:
+          PlayerY += 1
 
       Display.fill((0, 0, 0))
 
