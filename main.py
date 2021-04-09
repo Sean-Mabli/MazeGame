ac888b276f4e3c23a3ea96530006c468a2baf830
Sean Mabli <>
2021-04-09 01:31:38 +0000

    .

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import numpy as np
 
 Display = pygame.display.set_mode([500, 500])
-Screen = 'start'
+Screen = 'end'
 
 PlayerX = 0
 PlayerY = 0
@@ -17,11 +17,11 @@ while not done:
       done = True
     if(Screen == 'start'):
       Display.fill((0, 0, 0))
-      button = pygame.draw.rect(Display, (255, 255, 255), (150, 300, 200, 60),  2)
+      StartButton = pygame.draw.rect(Display, (255, 255, 255), (150, 300, 200, 60),  2)
       Display.blit(pygame.font.SysFont("Raleway", 70).render("Complete The Maze", 1, (255, 255, 255)), (15, 200))
       Display.blit(pygame.font.SysFont("Raleway", 40).render("Start Game", 1, (255, 255, 255)), (174, 317))
 
-      if(pygame.mouse.get_pressed()[0] == True and button.collidepoint(pygame.mouse.get_pos())):
+      if(pygame.mouse.get_pressed()[0] == True and StartButton.collidepoint(pygame.mouse.get_pos())):
         Screen = 'game'
     if(Screen == 'game'):
       if event.type == pygame.KEYDOWN:
@@ -37,6 +37,7 @@ while not done:
       Display.fill((0, 0, 0))
 
       Wall = np.ones((4, int(500 / 20), int(500 / 20)))
+      VisitedBoxes = np.ones((500, 500))
 
       for i in range(int(500 / 20)):
         for j in range(int(500 / 20)):
@@ -48,6 +49,14 @@ while not done:
             pygame.draw.line(Display, (255, 255, 255), (i * 20 + 20, j * 20 + 20), (i * 20 + 20, j * 20))
           if (Wall[3, i, j] == 1):
             pygame.draw.line(Display, (255, 255, 255), (i * 20 + 20, j * 20), (i * 20, j * 20))
+    if(Screen == 'end'):
+      Display.fill((0, 0, 0))
+      StartButton = pygame.draw.rect(Display, (255, 255, 255), (150, 300, 200, 60),  2)
+      Display.blit(pygame.font.SysFont("Raleway", 70).render("Game Over", 1, (255, 255, 255)), (115, 200))
+      Display.blit(pygame.font.SysFont("Raleway", 40).render("Play Again", 1, (255, 255, 255)), (180, 317))
+
+      if(pygame.mouse.get_pressed()[0] == True and StartButton.collidepoint(pygame.mouse.get_pos())):
+        Screen = 'game'
 
     pygame.display.flip()
 pygame.quit()
