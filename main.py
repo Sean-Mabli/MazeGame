c39b91cab4aadf2766fd79237cc694d4c8f178b7
Sean Mabli <>
2021-04-10 18:58:27 +0000

    .

--- main.py
+++ main.py
@@ -4,62 +4,66 @@ import numpy as np
 Display = pygame.display.set_mode([501, 501])
 Screen = 'start'
 
-PlayerX = 0
-PlayerY = 0
-
-CurrentX = 0
-CurrentY = 0
-
-Wall = np.ones((4, int(500 / 20), int(500 / 20)))
-VisitedBoxes = np.zeros((25, 25))
-Stack = []
-Stack.append((CurrentX, CurrentY))
+PlayerX = 23
+PlayerY = 24
 
 pygame.init()
 pygame.display.set_caption('Maze Game')
 
 done = False
 
-while(625 - np.sum(VisitedBoxes) != 0):
-  VisitedBoxes[CurrentX, CurrentY] = 1
-  OpenBoxes = np.zeros((4))
+def GenerateMaze():
+  CurrentX = 0
+  CurrentY = 0
+
+  Wall = np.ones((4, int(500 / 20), int(500 / 20)))
+  VisitedBoxes = np.zeros((25, 25))
+  Stack = []
   Stack.append((CurrentX, CurrentY))
 
-  if(VisitedBoxes[CurrentX, CurrentY - 1]  == 0 and CurrentY != 0): # Top
-    OpenBoxes[0] = 1
-  if(CurrentX <= 23):
-    if(VisitedBoxes[CurrentX + 1, CurrentY]  == 0): # Right
-      OpenBoxes[1] = 1
-  if(CurrentY <= 23):
-    if(VisitedBoxes[CurrentX, CurrentY + 1]  == 0): # Down
-      OpenBoxes[2] = 1
-  if(VisitedBoxes[CurrentX - 1, CurrentY]  == 0 and CurrentX != 0): # Right
-    OpenBoxes[3] = 1
+  while(625 - np.sum(VisitedBoxes) != 0):
+    VisitedBoxes[CurrentX, CurrentY] = 1
+    OpenBoxes = np.zeros((4))
+    Stack.append((CurrentX, CurrentY))
 
-  Random = np.random.randint(0, 4)
-  while (OpenBoxes[Random] != 1 and np.sum(OpenBoxes) != 0):
+    if(VisitedBoxes[CurrentX, CurrentY - 1]  == 0 and CurrentY != 0): # Top
+      OpenBoxes[0] = 1
+    if(CurrentX <= 23):
+      if(VisitedBoxes[CurrentX + 1, CurrentY]  == 0): # Right
+        OpenBoxes[1] = 1
+    if(CurrentY <= 23):
+      if(VisitedBoxes[CurrentX, CurrentY + 1]  == 0): # Down
+        OpenBoxes[2] = 1
+    if(VisitedBoxes[CurrentX - 1, CurrentY]  == 0 and CurrentX != 0): # Right
+      OpenBoxes[3] = 1
+
     Random = np.random.randint(0, 4)
+    while (OpenBoxes[Random] != 1 and np.sum(OpenBoxes) != 0):
+      Random = np.random.randint(0, 4)
 
-  if(sum(OpenBoxes) == 0 and len(Stack) != 1):
-    (CurrentX, CurrentY) = Stack.pop()
-    (CurrentX, CurrentY) = Stack.pop()
-  elif(sum(OpenBoxes) != 0 and len(Stack) != 1):
-    if(Random == 0):
-      Wall[0, CurrentX, CurrentY] = 0
-      Wall[2, CurrentX, CurrentY - 1] = 0
-      CurrentY -= 1
-    elif(Random == 1):
-      Wall[1, CurrentX, CurrentY] = 0
-      Wall[3, CurrentX + 1, CurrentY] = 0
-      CurrentX += 1
-    elif(Random == 2):
-      Wall[2, CurrentX, CurrentY] = 0
-      Wall[0, CurrentX, CurrentY + 1] = 0
-      CurrentY += 1
-    elif(Random == 3):
-      Wall[3, CurrentX, CurrentY] = 0
-      Wall[1, CurrentX - 1, CurrentY] = 0
-      CurrentX -= 1
+    if(sum(OpenBoxes) == 0 and len(Stack) != 1):
+      (CurrentX, CurrentY) = Stack.pop()
+      (CurrentX, CurrentY) = Stack.pop()
+    elif(sum(OpenBoxes) != 0 and len(Stack) != 1):
+      if(Random == 0):
+        Wall[0, CurrentX, CurrentY] = 0
+        Wall[2, CurrentX, CurrentY - 1] = 0
+        CurrentY -= 1
+      elif(Random == 1):
+        Wall[1, CurrentX, CurrentY] = 0
+        Wall[3, CurrentX + 1, CurrentY] = 0
+        CurrentX += 1
+      elif(Random == 2):
+        Wall[2, CurrentX, CurrentY] = 0
+        Wall[0, CurrentX, CurrentY + 1] = 0
+        CurrentY += 1
+      elif(Random == 3):
+        Wall[3, CurrentX, CurrentY] = 0
+        Wall[1, CurrentX - 1, CurrentY] = 0
+        CurrentX -= 1
+  return Wall
+
+Wall = GenerateMaze()
 
 while not done:
   for event in pygame.event.get():
@@ -98,9 +102,9 @@ while not done:
             pygame.draw.line(Display, (255, 255, 255), (i * 20 + 20, j * 20 + 20), (i * 20, j * 20 + 20))
           if (Wall[3, i, j] == 1):
             pygame.draw.line(Display, (255, 255, 255), (i * 20, j * 20 + 20), (i * 20, j *20))
-    
-    if(PlayerX == 24 and PlayerY == 24):
-      Screen == 'end'
+
+      if(PlayerX == 24 and PlayerY == 24):
+        Screen = 'end'
 
     if(Screen == 'end'):
       Display.fill((0, 0, 0))
